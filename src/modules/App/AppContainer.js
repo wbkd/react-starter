6276275c849e15593a229d2d48b37d984a0d2f9d
Stefan Keil <an.stefan.keil@googlemail.com>
2017-01-16 11:36:44 +0100

    add react router v4

--- src/modules/App/AppContainer.js
+++ src/modules/App/AppContainer.js
@@ -1,6 +1,22 @@
+
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter, Match } from 'react-router';
 import App from './App';
+
+
+class AppContainer extends PureComponent {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Match exactly pattern="/" component={App} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default connect(
   state => ({  ...state.AppState })
-)(App);
+)(AppContainer);
