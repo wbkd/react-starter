2359a39c260382b66246fe5d43ff9a01a44259e6
Stefan Keil <an.stefan.keil@googlemail.com>
2017-01-17 11:58:16 +0100

    add iframe test page

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -35,6 +35,12 @@ module.exports = {
       inject: true,
       template: Path.resolve(__dirname, '../src/index.html'),
     }),
+    new HtmlWebpackPlugin({
+      title: 'iframe testpage',
+      inject: false,
+      filename: 'index_iframe.html',
+      template: Path.resolve(__dirname, '../src/index_iframe.html'),
+    }),
     new CopyWebpackPlugin([
       { from:  Path.resolve(__dirname, '../src/public/data'), to: 'data' },
     ])
